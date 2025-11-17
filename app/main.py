da31d3ada7c7f17bbf1f24139a2f82e543803229
Matheus José <pwvmat@gmail.com>
2025-11-17 00:48:07 -0300

    Subindo adição do CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+
 from app.api.router import api_router
 
 # TODO: Lembrar de documentar melhor as classes, métodos e utilizar as docstrings para melhorar as descrições no Swagger
@@ -10,6 +12,21 @@ app = FastAPI(
     title="Fast Ariam API",
     description="API para o sistema de gerenciamento de Ordens de Serviço da Fast Ariam.",
     version="0.1.0" # Protótipo humildão
+)
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    # TODO: Adicionar a URL de produção
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
